c653b19816e5f73c2a897318ce0ee09e7f5d7344
santhitak <santhita.sk@gmail.com>
2025-02-08 13:25:11 +0700

    feat: add promptTab

---
+++ src/app/component/promptCard.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function PromptCard(
+    {prompt}: Readonly<{ prompt: string; }>
+) {
+    return (
+        <div
+            className="w-full h-5/6 border-zinc-300 border bg-white rounded-2xl hover:bg-white/20
+                        transition-all ease-in-out overflow-auto shadow-lg p-8 lg:p-14 flex justify-center items-center">
+            <p className="text-center font-medium text-xl">
+                {prompt}
+            </p>
+        </div>
+    )
+}
